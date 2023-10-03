eb0e21598213a6e6b9e0d9501a7c3333a0c92877
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-10-03 15:10:31 +0000

    added RCAV for user show page and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     render template: "users_templates/index"
   end
 
+  def show
+    @user = User.where({username: params.fetch("username")})[0]
+    render template: "users_templates/show"
+  end
+
   def create
     @user = User.new
     @user.username = params.fetch("username")
@@ -12,6 +17,19 @@ class UsersController < ApplicationController
       redirect_to "/users", {:notice => "User added successfully."}
     else
       redirect_to "/users", {:alert => @user.errors.full_messages.to_sentence}
+    end
+  end
+
+  def update
+    @user = User.where({id: params.fetch("user_id")})[0]
+
+    @user.username = params.fetch("username")
+
+    if @user.valid?
+      @user.save
+      redirect_to "/users/#{@user.username}", {:notice => "Username updated successfully."}
+    else
+      redirect_to "/users/#{@user.username}", {:alert => @user.errors.full_messages.to_sentence}
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # User routes
   get("/", { controller: "users", action: "index" })
   get("/users", { controller: "users", action: "index" })
+  get("/users/:username", {controller: "users", action: "show"})
+
   post("/add_user", {controller: "users", action: "create"})
+  post("/edit_user", {controller: "users", action: "update"})
+
+  # Photo routes
 end
